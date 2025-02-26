853c64b0677e921b7c2f7be9483f6836f043dfd3
sjhhh321 <1587557881@qq.com>
2025-02-26 15:49:41 +0800

    操作日志-响应体获取实现

--- middleware/log_middleware.go
+++ middleware/log_middleware.go
@@ -8,6 +8,15 @@ import (
 	"io"
 )
 
+type ResponseWriter struct {
+	gin.ResponseWriter
+	Body []byte
+}
+
+func (w *ResponseWriter) Write(data []byte) (int, error) {
+	w.Body = append(w.Body, data...)
+	return w.ResponseWriter.Write(data)
+}
 func LogMiddleware(c *gin.Context) {
 	// 请求中间件
 	byteData, err := io.ReadAll(c.Request.Body)
@@ -17,6 +26,11 @@ func LogMiddleware(c *gin.Context) {
 	fmt.Println("body: ", string(byteData))
 	// 这是因为c.Request.Body被使用后就会销毁，不能再绑定，所以需要重新赋值创建。
 	c.Request.Body = io.NopCloser(bytes.NewReader(byteData))
+	res := &ResponseWriter{
+		ResponseWriter: c.Writer,
+	}
+	c.Writer = res
 	c.Next()
 	// 响应中间件
+	fmt.Println("response: ", string(res.Body))
 }
